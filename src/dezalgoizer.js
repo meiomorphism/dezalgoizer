d64c921f586579010fc6e740912ffea609af67c8
meiomorphism <meiomorphism@gmail.com>
2020-11-24 01:32:05 -0800

    main: rewrite main Javascript file
    Despite changing almost every line of code, this is largely cleanup.
    
    The only real logic changes are:
    
      * '0' is no longer used as a sentinel value in the arguments to `f` (now named
        `dezalgoizeNodeSpan`); instead, it's computed when the other arguments are.
    
      * A sanity check in the DOM traversal code has been removed.
    
      * Some new combining characters have been added to the regex.
    
    Other than that, it's just modernization and polish:
    
      * Variables have all been renamed to something more readable, now that I'm not
        authoring code primarily by banging rocks together. Similarly, a couple of
        items that didn't need to be declared separately have been inlined.
    
      * ES6+ syntax is used everywhere. Mostly this is just `let`, `const`, and the
        occasional `for ... of`, but the spread operator also replaces a use of
        `Function.apply`.
    
      * A generator function -- not available in Chrome at the time -- is used to
        simplify DOM-traversal. Another is used to isolate some mutable state while
        traversing the selection-object.
    
      * The comments have all been touched up.

--- src/dezalgoizer.js
+++ src/dezalgoizer.js
@@ -1,80 +1,114 @@
 (() => {
-  var NO_SEL = "No text selected to dezalgoize!";
+  const selections = window.getSelection();
+  if (!selections) { alert("No text selected to dezalgoize!"); return; }
 
-  // Get the range object. (Currently FF-only.)
-  var sel = window.getSelection();
-  if (!sel) { alert(NO_SEL); return; }
-  var newRanges = [];
-  for (var selIx = 0; selIx < sel.rangeCount; ++selIx) {
-    var s = sel.getRangeAt(selIx);
-    if (!s || s.collapsed) continue;
+  // Convenience iterator over the selection range object.
+  //
+  // Firefox, almost uniquely among browsers, allows multiple selection ranges.
+  // (Hold Ctrl while selecting!) We mostly handle this correctly, but we do
+  // assume throughout that the selection ranges don't overlap. Firefox doesn't
+  // _guarantee_ that, but it's almost always true in practice, and it's rarely
+  // more than a cosmetic issue when it fails.
+  function* getSelections() {
+    for (let i = 0; i < selections.rangeCount; ++i) {
+      yield selections.getRangeAt(i);
+    }
+  }
 
-    // Match all combining characters in the Basic Multilingual Plane.
-    //
-    // (This omits the combining musical characters in the SMP: FF 26
-    // strips actual text if they're included. They also aren't yet
-    // widely supported, so there's probably no need to worry about
-    // them.)
-    var CCs = /[\u0300-\u036F]|[\u0483-\u0489]|[\u1DC0-\u1DFF]|[\u20D0-\u20FF]|[\uFE20-\uFE2F]/g;
+  // A regex matching the most common Zalgotext characters.
+  //
+  // This is not merely the regex `/\p{Mn}|\p{Me}/ug`. Instead, we limit this to
+  // characters which are in the Basic Multilingual Plane, and which are
+  // associated with either the Latin alphabet, the Cyrillic alphabet, or with
+  // no particular script at all.
+  //
+  // * We do not attempt to match astral combining characters, such as the
+  //   combining musical characters in the SMP, since some regex engines have
+  //   issues matching them. In Firefox 26, this would actually result in the
+  //   underlying text being removed! (This has likely been resolved since --
+  //   possibly by the introduction of the `u` flag -- but testing is needed to
+  //   confirm that.)
+  //
+  // * We do not attempt to match characters in scripts other than Latin and
+  //   Cyrillic because many scripts - notably, all of the Brahmic scripts -
+  //   rely on combining characters to form ordinary, day-to-day text. (We will,
+  //   alas, still destroy properly-encoded Yoruba -- but then again, so does
+  //   Zalgoification.
+  //
+  // At any rate, Zalgoifier writers seem to generally just use characters from
+  // those Unicode blocks designated specifically for combining characters, so
+  // complete coverage isn't necessary.
+  //
+  const combiningCharacters = /[\u0300-\u036F]|[\u0483-\u0489]|[\u1AB0-\u1AFF]|[\u1DC0-\u1DFF]|[\u20D0-\u20FF]|[\u2DE0-\u2DFF]|[\uA66F-\uA67D]|\uA69E|\uA69F|[\uFE20-\uFE2F]/ug;
 
-    // Strip all combining characters from a string, returning it.
-    function rr(T) { return T.replace(CCs, ""); }
+  // Accumulator for replacement selection-ranges. (This will be used to fix up
+  // the selection after we modify the text out from under it.)
+  const newRanges = [];
+
+  for (const selection of getSelections()) {
+    if (!selection || selection.collapsed) continue;
+
     // Strip all combining characters from a particular substring [a,b)
     // of a string.
-    function r(T, a, b) {
-      if (!b) b = T.length;
-      return T.slice(0, a) + rr(T.slice(a, b)) + T.slice(b);
+    function dezalgoizeSpan(str, a, b) {
+      return (
+        str.slice(0, a)
+        + str.slice(a, b).replace(combiningCharacters, "")
+        + str.slice(b)
+      );
     }
     // Strip all combining characters from a particular substring [a,b)
     // of a text-node's contents.
-    function f(N, a, b) { N.nodeValue = r(N.nodeValue, a, b); }
+    function dezalgoizeNodeSpan(node, a, b) {
+      node.nodeValue = dezalgoizeSpan(node.nodeValue, a, b);
+    }
 
-    // Accumulator: collection of selected text nodes to edit, along with
-    // their selected region.
-    var nodes = [];
-    // Shorthand aliases, yes, but also save the selection state
+    // Shorter aliases. Also, and far more importantly, save the selection state
     // for later restoration.
-    var NS = s.startContainer;
-    var NE = s.endContainer;
-    var OS = s.startOffset;
-    var OE = s.endOffset;
-    // End co-offset. For string nodes, the last few characters will be
-    // unchanged, so we count backwards from the end both times.
-    var OcE = 0;
-    if (NE.nodeValue) OcE = NE.nodeValue.length - OE;
+    const nodeS = selection.startContainer;
+    const nodeE = selection.endContainer;
+    const offsetS = selection.startOffset;
+    const offsetE = selection.endOffset;
+    // End co-offset: the distance from the end of the node's text to the end of
+    // the selection. (This computation will be inverted later to compute the
+    // correct selection location.)
+    const coOffsetE = nodeE.nodeValue != undefined
+      ? nodeE.nodeValue.length - offsetE
+      : 0;
 
-    // Accumulation function. We start at a local root and work our way down,
-    // discarding any irrelevant subtrees.
-    function SNs(N) {
-      if (!N) { console.log("called with null"); return; }
-      if (!s.intersectsNode(N)) return;
-      if (N.nodeValue) {
-        var delim = [N, 0, 0];
-        if (N == NS) delim[1] = OS;
-        if (N == NE) delim[2] = OE;
-        nodes.push(delim);
+    // DOM traversal. Yields arguments to `dezalgoizeNodeSpan`, above.
+    function* findSelectedSpans(node) {
+      if (!selection.intersectsNode(node)) return;
+      if (node.nodeValue) {
+        yield [
+          node,
+          node === nodeS ? offsetS : 0,
+          node === nodeE ? offsetE : node.nodeValue.length
+        ];
       } else {
-        for (var C = N.firstChild; C; C = C.nextSibling) SNs(C);
+        for (let C of node.childNodes)
+          yield* findSelectedSpans(C);
       }
     }
 
     // Run over all the nodes starting from a convenient root...
-    SNs(s.commonAncestorContainer);
-    // ... then alter each of the discovered text nodes in-place.
-    nodes.forEach(function (a) { f.apply(this, a) });
+    for (const span of findSelectedSpans(selection.commonAncestorContainer)) {
+      // ... and alter each of the (partially-)selected text nodes in-place.
+      dezalgoizeNodeSpan(...span)
+    }
 
-    // Calculate the new end-offset...
-    var OE2 = OE; if (NE.nodeValue) OE2 = NE.nodeValue.length - OcE;
-    // ... and save off the selection, to be restored once we've been
-    // through all the subselections. (This does assume that selection-
-    // ranges can't overlap.)
-    var newRange = new Range();
-    newRange.setStart(NS, OS);
-    newRange.setEnd(NE, OE2);
+    // Compute and save the new selection, to be restored once we've been through
+    // all the subselections.
+    const newOffsetE = nodeE.nodeValue != undefined
+      ? nodeE.nodeValue.length - coOffsetE
+      : offsetE;
+    const newRange = new Range();
+    newRange.setStart(nodeS, offsetS);
+    newRange.setEnd(nodeE, newOffsetE);
     newRanges.push(newRange);
   }
 
-  // Restore selections.
-  sel.removeAllRanges();
-  newRanges.forEach(function (r) { sel.addRange(r) });
+  // Reselect the previously-selected text.
+  selections.removeAllRanges();
+  for (const r of newRanges) { selections.addRange(r) };
 })();
